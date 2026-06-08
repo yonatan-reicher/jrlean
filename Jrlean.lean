4cf68f485afb99a65f936bdf335a531fa779e0cb
Jonathan Reicher <yony252525@gmail.com>
2026-06-08 11:03:33 +0300

    move to last

--- Jrlean.lean
+++ Jrlean.lean
@@ -1,8 +1,8 @@
 module
-public import Jrlean.Effect
 public import Jrlean.Coe
 public import Jrlean.FiniteType
 public import Jrlean.DMonad
 public import Jrlean.DReader
 public import Jrlean.DependentState
 public import Jrlean.C
+public import Jrlean.Effect
