7112a7850dfa12769396c929ea2b551e84e06c9f
Jonathan Reicher <yony252525@gmail.com>
2026-06-11 11:45:56 +0300

    update

--- Jrlean/TypeId.lean
+++ Jrlean/TypeId.lean
@@ -169,8 +169,41 @@ instance : DecidableEq TypeId := by
           grind only [List.getElem?_eq_none]
         rw [this.1, this.2]
 
+/-- A relation whether a type id matches a type. -/
+public opaque TypeId.OfType : TypeId → Type → Prop
+/-- Each type has an id. -/
+public axiom TypeId.exists_ofType t : ∃ id, OfType id t
+/-- Each type's id is unique. -/
+public axiom TypeId.eq_of_ofType {t id1 id2}
+: OfType id1 t → OfType id2 t → id1 = id2
+
+unsafe def TypeId.trustMe {α} {id : TypeId} : id.OfType α := unsafeCast True
+
+@[ext]
 public class HasTypeId (α : Type) where
   typeId : TypeId
+  h_correct : typeId.OfType α
+
+/-- All instances are equal. -/
+public theorem HasTypeId.unique (α : Type)
+: ∀ (inst1 inst2 : HasTypeId α), inst1 = inst2 := by
+  rintro ⟨id1, h1⟩ ⟨id2, h2⟩
+  ext1
+  show id1 = id2
+  exact TypeId.eq_of_ofType h1 h2
+
+/-- Every type can have an instance of `HasTypeId` constructed for it. -/
+public instance {α} : Nonempty (HasTypeId α) := by
+  open Classical in
+  let exists_id := TypeId.exists_ofType α
+  let id := choose exists_id
+  have : id.OfType α := by apply choose_spec exists_id
+  exact Nonempty.intro ⟨id, this⟩
+
+axiom TypeId.id_nat : OfType ⟨``Nat, [], []⟩ Nat
+instance : HasTypeId Nat where
+  typeId := ⟨``Nat, [], []⟩
+  h_correct := TypeId.id_nat
 
 -- Unfortunately, Lean does not implement this class for almost any type, so we
 -- have to do it ourselves. What a shame.
