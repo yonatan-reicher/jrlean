b04379c937905362d952f61353d938eccef4f859
Jonathan Reicher <yony252525@gmail.com>
2026-07-11 15:08:42 +0300

    add `instance ... infer`

--- Jrlean.lean
+++ Jrlean.lean
@@ -2,6 +2,7 @@ module
 public import Jrlean.Of
 public import Jrlean.SetTactic
 public import Jrlean.ByContra
+public import Jrlean.InstanceInfer
 public import Jrlean.CollectionLemmas
 public import Jrlean.UniqueName
 public import Jrlean.HasTypeId
---
+++ Jrlean/InstanceInfer.lean
@@ -0,0 +1,14 @@
+module
+
+meta import Lean.Parser
+meta import Lean.Parser.Command
+meta import Lean.Elab.Command
+meta import Lean.Parser.Term
+
+macro "instance " t:bracketedBinder* ": " typeClass:ident name:ident " infer" : command => do
+  -- TODO: Why can't I add visibility modifiers before the instance keywords?
+  `(
+    instance $t:bracketedBinder* : $typeClass $name := by
+      simp [$name:term]; split <;> infer_instance
+  )
+
