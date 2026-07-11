1281648a77cb7f632f472e4434a1f87f79303863
Jonathan Reicher <yony252525@gmail.com>
2026-07-11 20:36:09 +0300

    delete congruence file

--- Jrlean/Coc.lean
+++ Jrlean/Coc.lean
@@ -4,7 +4,6 @@ public import Jrlean.Coc.Var
 public import Jrlean.Coc.Basic
 public import Jrlean.Coc.Notation
 public import Jrlean.Coc.Repr
-public import Jrlean.Coc.CongruenceOf
 public import Jrlean.Coc.MoveIntoOutOf
 public import Jrlean.Coc.Beta
 public import Jrlean.Coc.BetaLemmas
