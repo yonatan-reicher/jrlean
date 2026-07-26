6427548e5da429efb0a6c9ab8d92d6879e1af5d5
Jonathan Reicher <yony252525@gmail.com>
2026-07-26 20:02:55 +0300

    reader monad

--- Jrlean/Later.lean
+++ Jrlean/Later.lean
@@ -1,5 +1,32 @@
 module
 
+-- Ours
 public import Jrlean.LaterEnvExtension
+-- Lean
+public import Lean.Elab.Tactic
+
+open Lean
+open Lean.Elab.Tactic
 
 namespace Jrlean
+
+-- Define the syntax
+syntax (name := laterTactic) "later" : tactic
+syntax (name := laterBlock) term:min " with " "laters " tacticSeq : tactic
+
+meta instance : MonadState (List LaterContext) TacticM where
+  get := return laterEnvExtension.getState (← getEnv)
+  set s := modifyEnv (laterEnvExtension.setState · s)
+  modifyGet f := do
+    let env ← getEnv
+    let s := laterEnvExtension.getState env
+    let (ret, s') := f s
+    let env' := laterEnvExtension.setState env s'
+    setEnv env'
+    return ret
+
+
+-- When we enter a `later` block, we push a new `LaterContext` onto the stack, and when done, we pop.
+private meta def onEnter (termGoal : MVarId) : TacticM Unit := do
+  let ctx := LaterContext.mk termGoal
+  modify (ctx :: ·)
