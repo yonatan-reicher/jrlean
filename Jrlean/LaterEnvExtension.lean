2e3185232037169630b8ce788ae51f7b1dcf77d8
Jonathan Reicher <yony252525@gmail.com>
2026-07-26 19:47:22 +0300

    add envextension

--- Jrlean/LaterEnvExtension.lean
+++ Jrlean/LaterEnvExtension.lean
@@ -2,4 +2,23 @@ module
 
 public import Lean
 
+open Lean
+
 namespace Jrlean
+
+public meta structure LaterContext where
+  /--
+  This is the meta variable that is used as a goal for the term returned by the current `later`
+  block.
+  -/
+  term : MVarId
+
+/--
+A global environment extension that stores the context needed for the `later` tactic and the `later`
+block. This is a list because these can be nested. When the list is empty, we are not inside a
+`later` block.
+-/
+meta initialize laterEnvExtension : EnvExtension (List LaterContext) ←
+  Lean.registerEnvExtension
+    (mkInitial := pure [])
+    (replay? := none) -- TODO: I have no idea what this does.
